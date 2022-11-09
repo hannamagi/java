245d10356e3474160f51ae9bee325906f33ec64d
Hanna <hanna.magi.001@voco.ee>
2022-11-09 15:12:47 +0200

    Muutujad

---
+++ Muutujad.js
@@ -0,0 +1,26 @@
+console.log("Hello World!")
+// variables
+// let
+let name = 'Hanna Mägi'
+name = 'New name'
+
+// const
+const surname = 'Mägi'
+// surname = 'Mägi' - error
+
+// arrays
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers)
+numbers.push(6)
+console.log(numbers)
+
+// objects
+const person = {
+    firstname: 'Hanna',
+    surname: 'Mägi'
+}
+console.log(person)
+console.log(person.firstname)
+console.log(person.surname)
+person.department = 'IKT'
+console.log(person
