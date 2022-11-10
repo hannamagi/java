5d7891c91d2ee8ccac4bc46d97afd50ac51d2f35
Hanna <hanna.magi.001@voco.ee>
2022-11-10 09:13:23 +0200

    Andmetüübid

---
+++ Andmetüübid.js
@@ -0,0 +1,45 @@
+// data types
+// String
+let name = 'Hanna Mägi'
+console.log(name)
+console.log(typeof name)
+
+let goodMark = 5
+console.log(goodMark)
+console.log(typeof goodMark)
+
+let persent = 50.3
+console.log(persent)
+console.log(typeof persent)
+
+let isHere = true // false
+console.log(isHere)
+console.log(typeof isHere)
+
+// arrays
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers)
+console.log(typeof numbers)
+
+// objects
+const person = {
+    firstname: 'Hanna',
+    surname: 'Mägi'
+}
+console.log(person)
+console.log(typeof person)
+
+// Null
+const airplane = null
+console.log(airplane)
+console.log(typeof airplane)
+
+// Symbol
+const sym = Symbol()
+console.log(sym)
+console.log(typeof sym)
+
+// Undefined
+let test
+console.log(test)
+console.log(typeof test)
