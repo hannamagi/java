26a9ce676bc2c8459cc56eff726c3c36c6bfcae2
Hanna <hanna.magi.001@voco.ee>
2022-11-15 14:50:45 +0200

    Massiivid

---
+++ Massiivid.js
@@ -0,0 +1,33 @@
+const numbers1 = [43, 56, 33, 23, 44, 36, 5]
+const numbers2 = new Array(22, 45, 33, 76, 54)
+
+let val
+
+val = numbers1.length
+val = numbers2.length
+
+val = numbers1[3]
+val = numbers1.indexOf(36)
+
+val = Array.isArray(numbers2)
+
+numbers1.push(250)
+numbers1.pop()
+numbers1.unshift(120)
+numbers1.shift()
+
+val = numbers1.concat(numbers2)
+
+const fruits = ['banana', 'apple', 'orange']
+val = fruits.sort()
+
+val = numbers1.sort(function (x, y) {
+    return x - y
+})
+
+
+val = numbers1.sort(function (x, y) {
+    return y - x
+})
+
+console.log(val)
